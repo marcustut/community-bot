495f9740e97899527641cc008b78d10fc8c2ddf4
neosizzle <44501267+neosizzle@users.noreply.github.com>
2021-04-29 09:53:46 +0800

    random colour and size generation added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,6 +7,43 @@ const client = new W3CWebSocket('ws://127.0.0.1:8000/ws/messages');
 const App: React.FC = () => {
   const [screen, setScreen] = useState<JSX.Element[]>([]);
 
+  //generates a random size and returns that said size as a string
+  //outputs 'small', 'medium' or 'large'
+  const generateRandomSize = ()=>{
+
+    const sizes = [
+      'small',
+      'medium',
+      'large',
+    ]
+
+    const index = Math.floor(Math.random() * sizes.length);
+    return sizes[index]
+
+  }
+
+   //generates a random colour and returns that said size as a string
+  //outputs rgb colour value
+  const generateRandomColour = ()=>{
+
+    const colours = [
+      'rgb(255, 129, 120)',
+      'rgb(213, 255, 161)',
+      'rgb(182, 255, 179)',
+      'rgb(179, 255, 242)',
+      'rgb(181, 214, 255)',
+      'rgb(146, 151, 247)',
+      'rgb(205, 167, 252)',
+      'rgb(205, 167, 252)',
+      'rgb(245, 171, 208)',
+      'rgb(255, 219, 120)'
+    ]
+
+    const index = Math.floor(Math.random() * colours.length);
+    return colours[index]
+
+  }
+
   useEffect(() => {
     setScreen(new BulletScreen('.screen', { duration: 20 }))
   }, [])
@@ -17,12 +54,13 @@ const App: React.FC = () => {
     client.onmessage = (message) => {
       const dataFromServer = JSON.parse(message.data as string);
       console.log('got reply! ', dataFromServer);
+      
       screen.push(
         <StyledBullet
           head={dataFromServer.avatarURL}
           msg={dataFromServer.text}
-          backgroundColor={'#fff'}
-          size='large'
+          backgroundColor={generateRandomColour()}
+          size={generateRandomSize()}
         />
       )
     }
